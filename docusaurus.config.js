84b97ec4cd6486e131308dfe095f7ad1cb0befd4
Mauriceac <maurice.ac@gmail.com>
2025-02-19 08:43:08 -0300

    Update edit URLs in Docusaurus configuration to point to the repository root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Mauriceac/recursos-ortodoxos/tree/main/docs',
+            'https://github.com/Mauriceac/recursos-ortodoxos/tree/main',
         },
         blog: {
           showReadingTime: true,
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Mauriceac/recursos-ortodoxos/tree/main/docs',
+            'https://github.com/Mauriceac/recursos-ortodoxos/tree/main',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
